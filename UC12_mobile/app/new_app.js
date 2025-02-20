2239948ecd0fbfb8a94373a0829a1d1911723c38
Leeh <leticiasalustiano07@gmail.com>
2025-02-20 09:37:16 -0300

    .

---
+++ UC12_mobile/app/new_app.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import { View, Text, Image } from 'react-native';
+
+//3 notícias com titulo e imagem
+
+class Noticias extends Component{
+    render(){
+
+        let titulo = 'Notícias'
+        
+    }
+}
